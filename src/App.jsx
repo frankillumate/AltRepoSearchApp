704822debbfe8b4da48475739e83d21e1920e3d5
Franklindev <franklin.jekwu@gmail.com>
2024-04-23 15:39:54 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./Home.jsx";
+import ErrorDisplay from "./Components/Error.jsx";
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route index path="/" element={<Home/>}/>
+    <Route index path="/home" element={<Home/>}/>
+    <Route path="*" element={<ErrorDisplay/>}/>
+   </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
